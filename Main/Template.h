42231f55dcd9a0363f13364998bde251641d32f2
gnoejh <hjeong@postech.ac.kr>
2025-09-26 09:15:56 +0500

    sfasfa

---
+++ Main/Template.h
@@ -0,0 +1,38 @@
+/*
+ * Template.h
+ *
+ * Created: 11/28/2021 9:36:39 AM
+ *  Author: hjeong
+ */ 
+
+
+#ifndef TEMPLATE_H_
+#define TEMPLATE_H
+
+//int main_template (void);
+int main_port (void);
+int main_graphics1 (void);
+int main_graphics2 (void);
+int main_graphics3 (void);
+int main_memory (void);
+int main_timer1 (void);
+int main_timer2 (void);
+int main_timer3 (void);
+int main_timer4 (void);
+int main_interrupt1 (void);
+int main_interrupt2 (void);
+int main_interrupt3 (void);
+int main_interrupt4 (void);
+int main_adc (void);
+int main_motors (void);
+int main_serial (void);
+int main_joystick (void);
+int main_accelerometr (void);
+int main_cds (void);
+int main_inline (void);
+int main_adc (void);
+int main_game_template (void);
+int main_game_obstacle (void);
+
+
+#endif /* TEMPLATE_H_ */
